bb6672f839e37ed29b8800d6de15005e706deb37
jonathanjoster <jonathanjoster0331@gmail.com>
2023-03-15 20:05:43 +0900

    add cli mode

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ import os
 import json
 
 FLAGS = flags.FLAGS
-flags.DEFINE_enum('lang', 'ja', ['ja', 'en'], 'ja / en')
+flags.DEFINE_enum('lang', 'en', ['ja', 'en'], 'ja / en')
+flags.DEFINE_bool('cli', True, 'feed text with CLI', short_name='c')
 
 CONFIG_PATH = './config.json'
 LOG_PATH = './abbi_log.txt'
@@ -21,34 +22,38 @@ def conversation(language: str):
 
     # ユーザーの入力を受け付ける
     try:
-        with sr.Microphone() as source:
-            listener = sr.Recognizer()
+        if FLAGS.cli:
+            _your_text = input('Listening: ')
+        else:
+            with sr.Microphone() as source:
+                listener = sr.Recognizer()
+                print("Listening...")
+                voice = listener.listen(source)
+                _lang = 'ja-JP' if language == 'ja' else 'en-US'
+                _your_text = listener.recognize_google(voice, language=_lang)
 
-            print("Listening...")
-            voice = listener.listen(source)
-            _lang = 'ja-JP' if language == 'ja' else 'en-US'
-            voice_text = listener.recognize_google(voice, language=_lang)
-            print('\nYou:', voice_text)
+        print('\nYou:', _your_text)
 
-            completion = openai.ChatCompletion.create(
-                model='gpt-3.5-turbo',
-                messages=[{'role': 'user','content': voice_text}]
-            )
-            content = completion['choices'][0]['message']['content']
-            output_text = content.replace('\n', '')
+        completion = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=[{'role': 'user','content': _your_text}]
+        )
+        content = completion['choices'][0]['message']['content']
+        output_text = content.replace('\n', '')
 
-            print("Bot:", output_text)
-            
-            # speech
-            tts = gtts.gTTS(output_text, lang=language)
-            tts.save('gTTS_out.mp3')
-            playsound.playsound('gTTS_out.mp3')
-            os.remove('gTTS_out.mp3')
-            
+        print("Bot:", output_text)
+        
+        # speech
+        tts = gtts.gTTS(output_text, lang=language)
+        tts.save('gTTS_out.mp3')
+        playsound.playsound('gTTS_out.mp3')
+        os.remove('gTTS_out.mp3')
+        
+        if os.path.exists(LOG_PATH):
             with open(LOG_PATH, 'a+') as f:
                 f.writelines([f'[{datetime.datetime.now().isoformat()}]\n',
-                              f'>>> {voice_text}\n',
-                              f'{output_text}\n\n'])
+                            f'>>> {_your_text}\n',
+                            f'{output_text}\n\n'])
     except Exception as e:
         print(e)
         
